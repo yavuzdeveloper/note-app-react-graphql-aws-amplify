75b4b23a4fb1214e1b136867228addaaf88e0641
Yavuz Tokus <yavuztokus2010@gmail.com>
2021-08-30 21:51:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+import { listNotes } from "./graphql/queries";
+import { API } from "aws-amplify";
+import { createNote as createNoteMutation } from "./graphql/mutations";
 
-function App() {
+Amplify.configure(config);
+
+const initialFormState = {
+  name: "",
+  description: "",
+};
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [formState, setFormState] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const createNote = async () => {
+    if (!formState.name || !formState.description) return;
+
+    await API.graphql({
+      query: createNoteMutation,
+      variables: {
+        input: formState,
+      },
+    });
+
+    setNotes([...notes, formState]);
+    setFormState(initialFormState);
+  };
+
+  const fetchNotes = async () => {
+    const apiData = await API.graphql({
+      query: listNotes,
+    });
+
+    setNotes(apiData.data.listNotes.items);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Note-App</h1>
+
+      <div>
+        <input
+          onChange={e => setFormState({ ...formState, name: e.target.value })}
+          placeholder="Note name"
+          value={formState.name}
+          style={{ marginTop: "10px", width: "400px", height: "25px" }}
+        />
+      </div>
+      <div>
+        {" "}
+        <input
+          onChange={e =>
+            setFormState({ ...formState, description: e.target.value })
+          }
+          placeholder="Description"
+          value={formState.description}
+          style={{ marginTop: "10px", width: "400px", height: "25px" }}
+        />
+      </div>
+      <div>
+        {" "}
+        <button
+          onClick={createNote}
+          style={{
+            marginTop: "20px",
+            width: "410px",
+            height: "30px",
+            color: "white",
+            backgroundColor: "blue",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Create Note
+        </button>
+      </div>
+
+      {notes?.map(item => (
+        <div className="note" key={item.id}>
+          <h5>Name: {item.name}</h5>
+          <p>Description: {item.description}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
